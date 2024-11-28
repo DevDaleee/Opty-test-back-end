18b8cdc6bda28e13440043c93461afdcfc8061f7
DevDaleee <marcosdalessandroclima@ufpi.edu.br>
2024-11-28 08:52:33 -0300

    feat: Auth imoplemented with modules, middlewares, jwt strategies and some fixes to app work property

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,22 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
-// import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { CashflowModule } from './cashflow/cashflow.module';
 
 @Module({
-  // AuthModule, 
-  imports: [PrismaModule, UserModule, CashflowModule],
+
+  imports: [PrismaModule, UserModule, CashflowModule, AuthModule],
   controllers: [AppController],
   providers: [
     AppService,
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: JwtAuthGuard,
-    // },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
-export class AppModule { }
+export class AppModule {}
--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,21 +1,16 @@
-// import {
-//   ExecutionContext,
-//   Injectable,
-//   UnauthorizedException,
-// } from '@nestjs/common';
-// import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
-// @Injectable()
-// export class LocalAuthGuard extends AuthGuard('local') {
-//   canActivate(context: ExecutionContext) {
-//     return super.canActivate(context);
-//   }
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+    canActivate(context: ExecutionContext) {
+        return super.canActivate(context);
+    }
 
-//   handleRequest(err, user) {
-//     if (err || !user) {
-//       throw new UnauthorizedException(err?.message);
-//     }
-
-//     return user;
-//   }
-// }
+    handleRequest(err, user) {
+        if (err || !user) {
+            throw new UnauthorizedException(err?.message);
+        }
+        return user;
+    }
+}
